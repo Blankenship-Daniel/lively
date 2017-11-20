0013711939228da26c2875f18f6719a88d8fbf66
Daniel Blankenship <daniel@goreact.com>
2017-11-20 13:03:20 -0700

    remove logs

--- src/app/reducers/active.ts
+++ src/app/reducers/active.ts
@@ -5,7 +5,6 @@ export const LOAD_ACTIVE_SONGS = 'LOAD_ACTIVE_SONGS';
 export function activeReducer(state = [], action) {
 	switch (action.type) {
 		case LOAD_ACTIVE_SONGS:
-			console.log('activeReducer(', action.payload, ')');
 			return action.payload;
 		default:
 			return state;
